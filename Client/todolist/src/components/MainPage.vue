c4ac13eb694f2c4348af9ac4d027efb9dbab6fe0
Dima-homeL <1988maksyutenko@gmail.com>
2020-12-16 16:57:28 +0200

    Added custom text and style.

---
+++ Client/todolist/src/components/MainPage.vue
@@ -0,0 +1,23 @@
+<template>
+    <div id="main">
+        <h1>{{ text }}</h1>
+    </div>
+</template>
+
+<script>
+export default {
+
+  name: 'Main',
+
+  data () {
+    return {
+      text: 'This is main page.'
+    }
+  }
+
+}
+</script>
+
+<style>
+
+</style>
